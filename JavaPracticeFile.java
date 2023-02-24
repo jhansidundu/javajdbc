49fe522468a53181ca88860a8bf1b1e9137f418e
jhansidundu <116087257+jhansidundu@users.noreply.github.com>
2023-02-24 19:24:01 +0530

    Create JavaPracticeFile.java
    this is first save of JavaPracticeFile

---
+++ JavaPracticeFile.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.math.*;
+import java.lang.*;
+
+class JavaPracticeFile {
+ public static void main(String args[]) {
+System.out.println("why geetha not listen to my words");
+}
+}
