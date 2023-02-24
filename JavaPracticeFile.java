e55adf935a6a7c15fed86b63c955eba7e7c4cda4
jhansidundu <116087257+jhansidundu@users.noreply.github.com>
2023-02-24 19:27:31 +0530

    Update JavaPracticeFile.java
    second commit of JavaPracticeFile

--- JavaPracticeFile.java
+++ JavaPracticeFile.java
@@ -5,5 +5,6 @@ import java.lang.*;
 class JavaPracticeFile {
  public static void main(String args[]) {
 System.out.println("why geetha not listen to my words");
+  System.out.println("he does not has heart and he gave irrespect to jhansi");
 }
 }
